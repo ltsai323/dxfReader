fb24ca2b2bb9906c89cf12a74af1d12142d7b403
ltsai323 <johnson20050@gmail.com>
2022-05-03 16:00:27 +0800

    update mypoint

--- mypoint.py
+++ mypoint.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-mesg='Shortest distance algorithm in mini gantry coordinate'
+
 
 class MyPoint(object):
   def __init__(self, x,y):
@@ -18,30 +18,10 @@ def ConvertToMiniGantry( point ):
   return MyPoint(newx,newy)
   
 def SortingPoints(pointList):
-  pool=[ p for p in pointList ]
-  p0=pool[0]
-  pool.pop(0)
-  return ShortedLength(p0,pool)
+  pDir = { idx:point for idx,point in enumerate(pointList) }
+  idxs = { idx:False for idx in pDir.keys() }
+  print(pDir)
   
-  
-def ShortedLength(currentPoint, PointPool):
-  if len(PointPool)==0: return []
-  
-
-  leng=9999.
-  idxFound=-1
-  for idx,nextPoint in enumerate(PointPool):
-    if currentPoint.Length(nextPoint) < leng:
-      leng=currentPoint.Length(nextPoint)
-      idxFound=idx
-  pointFound=PointPool[idxFound]
-  PointPool.pop(idxFound)
-  resultlist=ShortedLength( pointFound, PointPool )
-  resultlist.append(pointFound)
-  return resultlist
-  
-  
-
 if __name__ == "__main__":
   import sys
   if len(sys.argv) != 2: raise IOError('input a text file!')
@@ -50,8 +30,5 @@ if __name__ == "__main__":
   lines = [ line.strip() for line in open(sys.argv[1], 'r').readlines() if 'CIRCLE' in line ]
   points = ( TextPoint(line) for line in lines )
   convertedPoints = ( ConvertToMiniGantry(point) for point in points )
-  print(mesg)
-  for idx,cPoint in enumerate(SortingPoints(convertedPoints)):
-    print('No.%2d: %s'%(idx,cPoint))
-    if idx%5==5-1: print('---- 5 sep ----')
+  SortingPoints( convertedPoints )
   #for cPoint in convertedPoints: print('Point is : %s' % cPoint)
